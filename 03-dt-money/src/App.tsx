8a9ac3b43ef9087241fce4473b58279ca8a3e95f
Simão Neto <simaomenezesneto@gmail.com>
2024-03-07 20:47:20 +0000

    feat: Create the component Summary

--- 03-dt-money/src/App.tsx
+++ 03-dt-money/src/App.tsx
@@ -2,11 +2,13 @@ import { ThemeProvider } from 'styled-components'
 import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 import { Transactions } from './pages/Transactions'
+import { SummaryContainer } from './components/Summary/styles'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyle />
+      <SummaryContainer />
       <Transactions />
     </ThemeProvider>
   )
