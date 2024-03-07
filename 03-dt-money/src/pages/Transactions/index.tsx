dcd06ba55bd871d6ff02f7751f00fa070d859fc5
Simão Neto <simaomenezesneto@gmail.com>
2024-03-07 20:56:01 +0000

    feat: Create table transactions

--- 03-dt-money/src/pages/Transactions/index.tsx
+++ 03-dt-money/src/pages/Transactions/index.tsx
@@ -1,11 +1,40 @@
 import { Header } from '../../components/Header'
 import { Summary } from '../../components/Summary'
 
+import {
+  PriceHighlight,
+  TransactionsContainer,
+  TransactionsTable,
+} from './styles'
+
 export function Transactions() {
   return (
     <div>
       <Header />
       <Summary />
+
+      <TransactionsContainer>
+        <TransactionsTable>
+          <tbody>
+            <tr>
+              <td width="50%">Desenvolvimento de Site</td>
+              <td>
+                <PriceHighlight variant="income">R$ 12.568</PriceHighlight>
+              </td>
+              <td>Venda</td>
+              <td>13/02/2024</td>
+            </tr>
+            <tr>
+              <td width="50%">Hamburger</td>
+              <td>
+                <PriceHighlight variant="outcome">R$ 56.22</PriceHighlight>
+              </td>
+              <td>Alimentação</td>
+              <td>13/02/2024</td>
+            </tr>
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
     </div>
   )
 }
